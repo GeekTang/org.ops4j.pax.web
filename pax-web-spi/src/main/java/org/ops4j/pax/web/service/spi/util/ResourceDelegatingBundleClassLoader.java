c78a0a0265903bb2848f3f617d675890268aaed0
Ares Tang <ares.tang@gmail.com>
2014-12-03 23:40:49 +0800

    PAXWEB-758 cache the classloader information to improve the effeciency, especially for web service application

--- pax-web-spi/src/main/java/org/ops4j/pax/web/service/spi/util/ResourceDelegatingBundleClassLoader.java
+++ pax-web-spi/src/main/java/org/ops4j/pax/web/service/spi/util/ResourceDelegatingBundleClassLoader.java
@@ -20,11 +20,15 @@ package org.ops4j.pax.web.service.spi.util;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Enumeration;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Vector;
 
 import org.ops4j.pax.swissbox.core.BundleClassLoader;
 import org.osgi.framework.Bundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A bundle class loader which delegates resource loading to a list
@@ -33,8 +37,24 @@ import org.osgi.framework.Bundle;
  * @author Harald Wellmann
  */
 public class ResourceDelegatingBundleClassLoader extends BundleClassLoader {
+	
+  private static final Logger LOG = LoggerFactory
+			.getLogger(ResourceDelegatingBundleClassLoader.class);
 
   private List<Bundle> bundles;
+  
+  private static ThreadLocal<MRUCache<String, Enumeration<URL>>> mruCache = new ThreadLocal<MRUCache<String, Enumeration<URL>>>();
+
+  static {
+	  String cacheSizeValue = System.getProperty("pax.web.classloader.cache.size", "500");
+	  try{
+		  cacheSize = Long.parseLong(cacheSizeValue);
+	  }catch(NumberFormatException e){
+		  LOG.warn("Properties pax.web.classloader.cache.size: {} is not a valid value.", System.getProperty("pax.web.classloader.cache.size"));
+	  }
+  }
+  
+  private static long cacheSize = 500;
 
   public ResourceDelegatingBundleClassLoader(List<Bundle> bundles) {
     super(bundles.get(0));
@@ -70,21 +90,77 @@ public class ResourceDelegatingBundleClassLoader extends BundleClassLoader {
 
   @Override
   protected Enumeration<URL> findResources(String name) throws IOException {
-    Vector<URL> resources = new Vector<URL>();
+	Enumeration<URL> url = getFromCache(name);
+	if(url != null){
+		return url;
+	}else{
+		Vector<URL> resources = new Vector<URL>();
 
-    for (Bundle delegate : bundles) {
-      try {
-        Enumeration<URL> urls = delegate.getResources(name);
-        if (urls != null) {
-          while (urls.hasMoreElements()) {
-            resources.add(urls.nextElement());
-          }
-        }
-      } catch (IllegalStateException exc) {
-        // ignore
-      }
-    }
+	    for (Bundle delegate : bundles) {
+	      try {
+	        Enumeration<URL> urls = delegate.getResources(name);
+	        if (urls != null) {
+	          while (urls.hasMoreElements()) {
+	            resources.add(urls.nextElement());
+	          }
+	        }
+	      } catch (IllegalStateException exc) {
+	        // ignore
+	      }
+	    }
 
-    return resources.elements();
+	    url = resources.elements();
+	    addToCache(name, url);
+	    return url;
+	}
+    
+  }
+  
+
+  
+  protected void addToCache(String name, Enumeration<URL> urls){
+	  if(mruCache.get() == null){
+		  mruCache.set(new MRUCache<String, Enumeration<URL>>(cacheSize));
+	  }
+	  
+	  mruCache.get().put(name, urls);
+  }
+  
+  protected Enumeration<URL> getFromCache(String name){
+	  Enumeration<URL> url = null;
+	  if(mruCache.get() != null){
+		  url = mruCache.get().get(name);
+	  }
+	  
+	  return url;
+  }
+  
+  public class MRUCache<K,V> extends LinkedHashMap<K,V>{
+	  
+    /**
+     * The default initial capacity - MUST be a power of two.
+     */
+    static final int DEFAULT_INITIAL_CAPACITY = 1 << 4; // aka 16
+
+    /**
+     * The load factor used when none specified in constructor.
+     */
+    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    
+    private static final long serialVersionUID = -3903517976398799492L;
+	
+	private static final long MAX_ENTRIES = 100;
+	
+	private long max = MAX_ENTRIES;
+    
+	public MRUCache(long maxCapacity){
+	  super(DEFAULT_INITIAL_CAPACITY, DEFAULT_LOAD_FACTOR, true);
+	  max = maxCapacity;
+	}
+
+	protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
+	   return size() > max;
+	}
+	  
   }
 }
